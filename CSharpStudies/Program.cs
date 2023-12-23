ecf7d91d3ac49acca18d2ba4844ece87e584a87e
George <oldlizard1337@gmail.com>
2023-12-23 23:56:12 +0300

    Add project files.

---
+++ CSharpStudies/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запрос ввода двух чисел
+        Console.WriteLine("Input two numbers:");
+
+        // Считываем ввод пользователя и сохраняем в переменные
+        double number1 = Convert.ToDouble(Console.ReadLine());
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        // Вывод чисел
+        Console.WriteLine($"Number 1: {number1}");
+        Console.WriteLine($"Number 2: {number2}");
+
+        // Сумма
+        double sum = number1 + number2;
+        Console.WriteLine($"Sum: {sum}");
+
+        // Разность
+        double difference = number1 - number2;
+        Console.WriteLine($"Residue: {difference}");
+
+        // Умножение
+        double product = number1 * number2;
+        Console.WriteLine($"Multiplication: {product}");
+
+        // Деление (проверка деления на 0)
+        if (number2 != 0)
+        {
+            double division = number1 / number2;
+            Console.WriteLine($"Division: {division}");
+        }
+        else
+        {
+            Console.WriteLine("You can't devide by zero.");
+        }
+
+        // Чтобы программа не завершалась сразу после вывода результата
+        Console.ReadLine();
+    }
+}
