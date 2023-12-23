9c71c4ddf5bf840d3bc3c0eab28926274b10dc35
George <oldlizard1337@gmail.com>
2023-12-24 00:52:20 +0300

    Task 4 added

---
+++ Task3/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+class User
+{
+    // Метод авторизации пользователя, возвращает привилегию
+    public Privilege Authorization()
+    {
+        Console.WriteLine("Установка прав пользователя.");
+        return new Privilege(); // Возвращает новый экземпляр привилегии
+    }
+
+    // Другие методы класса User
+    public void CreateOrder()
+    {
+        Console.WriteLine("Создание заказа.");
+    }
+
+    public void DeleteOrder()
+    {
+        Console.WriteLine("Удаление заказа.");
+    }
+
+    public void SendOrder()
+    {
+        Console.WriteLine("Отправка заказа.");
+    }
+}
+
+class Customer : User { } // Подклассы пользователя
+
+class Admin : User { }
+
+class Seller : User { }
+
+class Privilege
+{
+    public int Code { get; private set; } // Код привилегии
+
+    public Privilege()
+    {
+        Code = 0; // Устанавливаем значение по умолчанию
+    }
+
+    // Установка кода привилегии
+    internal void SetCode(int code)
+    {
+        Code = code;
+    }
+}
+
+class PrivilegeMaster
+{
+    // Получение привилегии в зависимости от типа пользователя
+    public Privilege GetPrivilege(User user)
+    {
+        Privilege privilege = new Privilege(); // Создаем новую привилегию
+
+        if (user is Admin)
+        {
+            privilege.SetCode(3); // Устанавливаем код 3 для админа
+        }
+        else if (user is Customer)
+        {
+            privilege.SetCode(2); // Устанавливаем код 2 для клиента
+        }
+        else if (user is Seller)
+        {
+            privilege.SetCode(1); // Устанавливаем код 1 для продавца
+        }
+
+        return privilege; // Возвращаем привилегию с установленным кодом
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем экземпляры различных пользователей
+        User customer = new Customer();
+        User admin = new Admin();
+        User seller = new Seller();
+
+        PrivilegeMaster privilegeMaster = new PrivilegeMaster(); // Создаем экземпляр PrivilegeMaster
+
+        // Пример использования
+        Privilege customerPrivilege = privilegeMaster.GetPrivilege(customer); // Получаем привилегию для клиента
+        Console.WriteLine($"Привилегия для клиента: {customerPrivilege.Code}");
+
+        Privilege adminPrivilege = privilegeMaster.GetPrivilege(admin); // Получаем привилегию для админа
+        Console.WriteLine($"Привилегия для админа: {adminPrivilege.Code}");
+
+        Privilege sellerPrivilege = privilegeMaster.GetPrivilege(seller); // Получаем привилегию для продавца
+        Console.WriteLine($"Привилегия для продавца: {sellerPrivilege.Code}");
+    }
---
+++ Task4/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+// Интерфейс для входа и выхода из системы
+interface IUser
+{
+    void Login();
+    void Logout();
+}
+
+// Абстрактный класс, реализующий интерфейс IUser
+abstract class AUser : IUser
+{
+    public bool IsBlocked { get; protected set; }
+
+    // Виртуальный метод отправки сообщения
+    public virtual bool SendMessage(IUser user, string text)
+    {
+        Console.WriteLine("Отправка сообщения...");
+        return true;
+    }
+
+    // Статический метод возврата всех типов пользователей
+    public static Type[] GetAllKindOfUsers()
+    {
+        return new Type[] { typeof(Admin), typeof(Customer) };
+    }
+
+    // Реализация методов интерфейса IUser
+    public void Login()
+    {
+        Console.WriteLine("Вход в систему...");
+    }
+
+    public void Logout()
+    {
+        Console.WriteLine("Выход из системы...");
+    }
+}
+
+// Класс заказа Order
+class Order
+{
+    public static int TotalOrders { get; private set; }
+
+    public Order()
+    {
+        TotalOrders++;
+    }
+}
+
+// Класс Admin, наследующий AUser
+class Admin : AUser
+{
+    public bool BlockUser(IUser user)
+    {
+        if (user is AUser aUser)
+        {
+            aUser.IsBlocked = true;
+            return true;
+        }
+        return false;
+    }
+}
+
+// Класс Customer, наследующий AUser
+class Customer : AUser
+{
+    public Order CreateOrder()
+    {
+        return new Order();
+    }
+
+    public override bool SendMessage(IUser user, string text)
+    {
+        if (user is Customer)
+        {
+            Console.WriteLine("Невозможно отправить сообщение другому клиенту.");
+            return false;
+        }
+        return true;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Пример использования классов
+        Admin admin = new Admin();
+        Customer customer1 = new Customer();
+        Customer customer2 = new Customer();
+        Order order1 = customer1.CreateOrder(); // order = new Order();
+
+        bool success = customer1.SendMessage(admin, "help me"); // success = true
+        Console.WriteLine($"Success 1: {success}");
+
+        success = customer1.SendMessage(customer2, "hi"); // success = false
+        Console.WriteLine($"Success 2: {success}");
+
+        success = admin.BlockUser(customer1); // success = true, customer1.IsBlocked = true
+        Console.WriteLine($"Success 3: {success}");
+
+        Order order2 = customer1.CreateOrder(); // order = null
+        Order order3 = customer2.CreateOrder(); // order = new Order();
+
+        Console.WriteLine($"Total orders: {Order.TotalOrders}"); // Всего было сделано: 2 заказа
+    }
+}
